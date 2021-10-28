823284e94485969ad3192889f0e87e62c4c907e1
jay <jay.mm.message@gmail.com>
2021-10-28 20:55:48 +0800

    Modify App.js to "Stateless Component"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react'
 
-class App extends React.Component {
-    render() {
-        return(
-            <div>
-                <hi>
-                    開始 React ^_^ ya
-                </hi>
-            </div>
-        )
-    }
+function App(props) {
+    return(
+        <div>
+            <hi>
+                開始 React ^_^ ya
+            </hi>
+        </div>
+    )
 }
 
 export default App
