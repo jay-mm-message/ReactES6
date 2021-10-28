b27aac8c35051162f9eb3c1d0ea7df731cf7da9e
jay <jay.mm.message@gmail.com>
2021-10-28 21:03:53 +0800

    Fixed issue. ( Module build failed, unexpected token.)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const App = (props) => {
+const App = (props) => (
     <div>
         <hi>
             開始 React ^_^ ya ( pure functoin ).
         </hi>
     </div>
-}
+)
+
 
 export default App
